960dcd16174ddfa1b0c22dec58e02f56fa942dbd
Amirhoseinmohammadi <amirmi951@gmail.com>
2025-09-06 11:30:29 +0330

    third commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,25 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "../styles/index.css";
-
-const inter = Inter({ subsets: ["latin"] });
+// import localFont from "next/font/local";
+// const shabnamFont = localFont({
+//   src: [
+//     {
+//       path: "../public/fonts/Shabnam-Light.woff2",
+//       weight: "300",
+//       style: "normal",
+//     },
+//     {
+//       path: "../public/fonts/Shabnam-Regular.woff2",
+//       weight: "400",
+//       style: "normal",
+//     },
+//   ],
+//   variable: "--font-shabnam",
+// });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -21,7 +36,7 @@ export default function RootLayout({
       */}
       <head />
 
-      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+      <body className={`bg-[#FCFCFC] dark:bg-black`}>
         <Providers>
           <Header />
           {children}
