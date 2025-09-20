dea1951ff7493edd5f75c384b47f849bca7b0cc8
AmirHM78 <amirmi951@gmail.com>
2025-09-20 13:51:48 +0330

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
 import Products from "@/components/Products";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
+
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
-  // other metadata
+  title: "ّIran Steps",
+  description: "This is Home for kids shoes",
 };
 
 export default function Home() {
@@ -23,14 +15,6 @@ export default function Home() {
       <ScrollUp />
       <Hero />
       <Products />
-      {/* <Video /> */}
-      {/* <Brands /> */}
-      <AboutSectionOne />
-      {/* <AboutSectionTwo /> */}
-      {/* <Testimonials /> */}
-      {/* <Pricing /> */}
-      {/* <Blog /> */}
-      {/* <Contact /> */}
     </>
   );
 }
