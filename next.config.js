5e6de459be22554effa5c175ed48cc9b8b03ca5a
AmirHM78 <amirmi951@gmail.com>
2025-10-14 18:13:07 +0330

    final

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    optimizeCss: true,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 module.exports = nextConfig;
