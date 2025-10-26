f2c5942ca173e432b8d64253a7b09edb817c60c4
AmirHM78 <amirmi951@gmail.com>
2025-10-26 14:12:50 +0330

    not found

---
+++ src/app/_not-found.tsx
@@ -0,0 +1,8 @@
+// app/_not-found.tsx
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">404 - صفحه یافت نشد</h1>
+    </div>
+  );
+}
