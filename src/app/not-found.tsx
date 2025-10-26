6e6e13f1d1540c805cb558500d2f798c851ffa9a
AmirHM78 <amirmi951@gmail.com>
2025-10-26 13:55:58 +0330

    fix

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { notFound } from "next/navigation";
-
-export default function Page() {
-  const someCondition = false;
-
-  if (!someCondition) notFound();
-
-  return <div>این صفحه وجود دارد</div>;
-}
