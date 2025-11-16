4e47f04add1e1634442c61f1c59d77607cc63f59
AmirHM78 <amirmi951@gmail.com>
2025-11-16 15:56:36 +0330

    fix context

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useCart } from "@/context/CartContext";
+import { useCart } from "@/contexts/CartContext";
 import { useToast } from "@/contexts/ToastContext";
 import Image from "next/image";
 import Link from "next/link";
